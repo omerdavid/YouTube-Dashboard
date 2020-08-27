54e49526742c8e16ba31fc5d32602ba7077c6144
omer <omerdavid@gmail.com>
2020-08-27 18:51:17 +0300

    first upload

---
+++ client/src/app/pages/tables/tables.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: "app-tables",
+  templateUrl: "tables.component.html"
+})
+export class TablesComponent implements OnInit {
+  columns: Array<any> = [{
+    header: 'Name',
+    field: 'name'
+}, {
+    header: 'Type',
+    field: 'type'
+}, {
+    header: 'Price',
+    field: 'price'
+}];
+
+source: Array<any> = [{
+  name: 'T-shirt',
+  type: 'clothes',
+  price: '15$'
+}, {
+  name: 'Shoes',
+  type: 'footwear',
+  price: '100$'
+}, {
+  name: 'Ball cap',
+  type: 'headgear',
+  price: '50$'
+}];
+res:any;
+  constructor(private httpClient:HttpClient) {}
+
+  ngOnInit() {
+    let apiKey='AIzaSyC__yvQu993fx_kxyW9GzAeqwe5MWO3seQ';
+let url=`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=ויטמיקס&key=${apiKey}`;
+//let getRatingUrl=`https://www.googleapis.com/youtube/v3/videos/getRating?id=-JQB_NeNLBU&key=${apiKey}`;
+// Authorization: Bearer [YOUR_ACCESS_TOKEN]
+// Accept: application/json
+
+    this.httpClient.get(url).subscribe(d=>{
+     this.res=d['items'].map(v=>v);
+     console.log(this.res);
+    });
+  }
+}
---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const  passport  =  require('passport');
+const  LocalStrategy  =  require('passport-local').Strategy;
+const app = new express();
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        if(username === "admin" && password === "admin"){
+            return done(null, username);
+        } else {
+            return done("unauthorized access", false);
+        }
+    }
+));
+passport.serializeUser(function(user, done) {
+    if(user) done(null, user);
+});
+  
+passport.deserializeUser(function(id, done) {
+    done(null, id);
+});
+
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next()
+    }
+    return res.status(400).json({"statusCode" : 400, "message" : "not authenticated"})
+}
+const auth = () => {
+    return (req, res, next) => {
+        passport.authenticate('local', (error, user, info) => {
+            if(error) res.status(400).json({"statusCode" : 200 ,"message" : error});
+            req.login(user, function(error) {
+                if (error) return next(error);
+                next();
+            });
+        })(req, res, next);
+    }
+}
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/authenticate', auth(),(req, res) => {
+    res.status(200).json({"statusCode" : 200 ,"message" : "hello"});
+});
+app.get('/getData', isLoggedIn, (req, res) => {
+    res.json("data")
+})
+app.get('/test',(req, res) => {
+    res.status(200).json({"statusCode" : 200 ,"message" : "hello"});
+});
+app.listen(3000, () => {
+    console.log('App running at 3000')
+})
