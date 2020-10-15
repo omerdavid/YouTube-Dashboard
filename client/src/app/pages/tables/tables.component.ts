14380b2524dba230b519559cceb8e32763e60635
omerdavid@gmail.com <omerdavid@gmail.com>
2020-10-15 10:21:37 +0300

    set table columns names

--- client/src/app/pages/tables/tables.component.ts
+++ client/src/app/pages/tables/tables.component.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ConfirmationService, MessageService } from 'primeng/api';
+
 export interface Car {
   id?;
   vin?;
@@ -13,25 +14,9 @@ export interface Car {
   price?;
   saleDate?;
 }
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+
+
 @Component({
   selector: "app-tables",
   templateUrl: "tables.component.html",
@@ -39,22 +24,22 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./tables.component.scss']
 })
 export class TablesComponent implements OnInit {
-  public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  public dataSource = ELEMENT_DATA;
-  res:Car[]= [
-    {"brand": "VW", "year": 2012, "color": "Orange", "vin": "dsad231ff","id":0},
-    {"brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345","id":0},
-    {"brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr","id":0},
-    {"brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh","id":0},
-    {"brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34","id":0},
-    {"brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj","id":0},
-    {"brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr","id":0},
-    {"brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34","id":0},
-    {"brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5","id":0},
-    {"brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s","id":0}
-]
+  public displayedColumns: string[] = ['videoId', 'title', 'channelTitle', 'description'];
+ 
+//   res:Car[]= [
+//     {"brand": "VW", "year": 2012, "color": "Orange", "vin": "dsad231ff","id":0},
+//     {"brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345","id":0},
+//     {"brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr","id":0},
+//     {"brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh","id":0},
+//     {"brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34","id":0},
+//     {"brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj","id":0},
+//     {"brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr","id":0},
+//     {"brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34","id":0},
+//     {"brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5","id":0},
+//     {"brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s","id":0}
+// ]
 cols: any[];
-//res:any[];
+res:any[];
 selectedProducts:any[];
 product:any;
 submitted:boolean;
@@ -68,12 +53,12 @@ productDialog: boolean;
   ngOnInit() {
 
 let keyword='גרעיני עפולה';
-    // this.httpClient.get(`api/youTubeList?searchText=${keyword}`)
-    // .pipe(map((r:any)=>r.res1.map((f:any)=>f.snippet)),catchError(this.errorHandler))
-    // .subscribe(d=>{
-    // // this.res=d;
-    //  console.log(this.res);
-    // });
+    this.httpClient.get(`api/youTubeList?searchText=${keyword}`)
+    .pipe(map((r:any)=>r.res1.map((f:any)=>{return {videoId:f.id.videoId, title:f.snippet.title,channelTitle:f.snippet.channelTitle,description:f.snippet.description}})),catchError(this.errorHandler))
+    .subscribe(d=>{
+     this.res=d;
+     console.log(this.res);
+    });
   }
   deleteSelectedProducts() {
     this.confirmationService.confirm({
